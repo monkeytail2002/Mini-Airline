b769c812cc31cbcbb6cb7600087d813182779ef6
Angus MacDonald <43272904+monkeytail2002@users.noreply.github.com>
2018-09-14 15:51:39 +0100

    Miniairline v1.2
    Added seat booking ability for option 1

--- Miniairline.java
+++ Miniairline.java
@@ -1,12 +1,47 @@
 import java.util.Scanner;
 public class Miniairline {
 
-	
-	
-	public static void main(String[] args) {
+public static void bookSeat(String pname[], int bookedseat, String name)
+{
+	pname[bookedseat] = name;
+	System.out.println("Your seat has been booked");
+}
+
+public static int getNumber()
+{
+	Scanner kboard = new Scanner(System.in);
+	int seat =0;
+	System.out.println("Please provide the seat number");
+	seat = kboard.nextInt();
+	return (seat);
+}
+
+public static String getName()
+{
+	Scanner kboard = new Scanner(System.in);
+	String name = "";
+	System.out.println("Please provide passenger name");
+	name = kboard.nextLine();
+	return (name);
+}
+
+
+public static void displaySeat(int noseats, String pname[])
+{
+	for  (int x = 1;x<noseats;x++)
+		System.out.println("Seat number: "+x+" Passenger name: "+pname[x]);
+}
+
+
+	public static void main(String[] args) 
+	{
 		// TODO Auto-generated method stub
-		Scanner kboard = new Scanner(System.in);
-		String choice="";
+        final int nosseats=15;
+        Scanner kboard = new Scanner(System.in);
+		String pname[] = new String[nosseats];
+		int ptype[] = new int[nosseats];
+		int choice = 0;
+
 		
 		
 System.out.println("Booking Menu");
@@ -16,8 +51,26 @@ System.out.println("2. Display Booked Seats");
 System.out.println("3. Cancel Booking");
 System.out.println("4. Takings");
 System.out.println("5. Exit");
-choice=kboard.nextLine();
-
+System.out.println("Please enter choice (press 8 to continue)");
+choice=kboard.nextInt();
+do
+{
+	switch(choice)
+	{
+	case 1: displaySeat(nosseats, pname);
+	bookSeat(pname, getNumber(), getName());
+	break;
 	}
-
+	System.out.println("Booking Menu");
+	System.out.println("Select from the options below");
+	System.out.println("1. Book Seat");
+	System.out.println("2. Display Booked Seats");
+	System.out.println("3. Cancel Booking");
+	System.out.println("4. Takings");
+	System.out.println("5. Exit");
+	System.out.println("Please enter choice (press 8 to continue)");
+	choice=kboard.nextInt();
+	}
+while (choice !=8);
+}
 }
