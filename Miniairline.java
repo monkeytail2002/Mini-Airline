cc81605a7cf8104d5b8a492bdf721b8266d2a04f
Angus MacDonald <43272904+monkeytail2002@users.noreply.github.com>
2018-09-14 15:15:49 +0100

    Add files via upload
    Version 1 - Creating a booking menu shell.

---
+++ Miniairline.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class Miniairline {
+
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner kboard = new Scanner(System.in);
+		String choice="";
+		
+		
+System.out.println("Booking Menu");
+System.out.println("Select from the options below");
+System.out.println("1. Book Seat");
+System.out.println("2. Display Booked Seats");
+System.out.println("3. Cancel Booking");
+System.out.println("4. Takings");
+System.out.println("5. Exit");
+choice=kboard.nextLine();
+
+	}
+
+}
